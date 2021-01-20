7568671f95c4f405f03a625b4ca06159f9cbb5d6
jefferymason <jefferymason72@gmail.com>
2021-01-20 08:28:19 -0600

    Initial Commit

---
+++ TestProjectAM/TestProjectAMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectAMApp.swift
+//  TestProjectAM
+//
+//  Created by Jeff Mason on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectAMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
